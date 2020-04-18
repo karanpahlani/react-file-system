bf87b2793e86a34667931921361e6421f9ef6611
Karan Pahlani <pahlani@gmail.com>
2020-04-17 21:58:21 -0500

    Changes in UI

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Filetree from './components/Filetree'
 
 function App() {
   return (
-    <Fragment>
-      <h1>File System</h1>
-      <h2>Files and Folders list</h2>
+    <div>
+
+      <h1 align="center" >File System</h1>
+      <h2 align="center"> Files and Folders list</h2>
       <h3>Root Directory</h3>
       
-      <Filetree files =  {{"folder1": {"blah": {}, "dubra": ""} , "folder2": "", "sds": {}}} />
-      </Fragment>
+      <Filetree files =  {{"My Folder": {"New Folder": {}, "Text File": ""}, "Other": {} , "File 2": ""}} />
+      
+    </div>
       
   );
 }
--- src/components/Filetree.js
+++ src/components/Filetree.js
@@ -1,5 +1,8 @@
  import React from 'react';
  import FolderIcon from '@material-ui/icons/Folder';
+ import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+ import Grid from '@material-ui/core/Grid'
  import DescriptionIcon from '@material-ui/icons/Description'
 
 class Filetree extends React.Component{
@@ -9,7 +12,8 @@ class Filetree extends React.Component{
         console.log(props.files);
         this.state = {
             files: this.props.files || {} ,
-            value: null
+            value: null,
+            newName: null
         }
         this.addFile = this.addFile.bind(this)
         this.addFolder = this.addFolder.bind(this)
@@ -21,7 +25,16 @@ class Filetree extends React.Component{
         this.setState({value: event.target.value});
 
       }
-    
+
+      handleChangeRename(event){
+        this.setState({value: event.target.newName});
+      }
+
+    renameFile(file){
+        var temp = this.state.files;
+        delete temp [file]
+        this.setState({ files: temp });
+    }
 
     addFile(event){
         var temp = this.state.files;
@@ -52,7 +65,7 @@ class Filetree extends React.Component{
         const files = this.state.files;
         return (
             <div>
-                <form onSubmit={this.handleSubmit} style={{ paddingLeft: '50px' , paddingBottom: '7px', paddingTop: '7px'}}>
+                <form onSubmit={this.handleSubmit} style={{ paddingLeft: '50px' , paddingBottom: '17px', paddingTop: '7px'}}>
                     <label>
                         Name:
                         <input type="text" value={this.state.value} onChange={this.handleChange} />
@@ -61,20 +74,37 @@ class Filetree extends React.Component{
                         <button type="submit" name="Add File" onClick= {this.addFile} >Add File</button>
                 </form>
                     
-
+                <Grid container spacing={3} direction="row">
                 <ul style={{ listStyleType: "none" }} >
                     {
                         Object.keys(files).map( file => {
                             if (typeof files[file] !== 'object'){
-                                return <li  > <DescriptionIcon/> {file} <button name="Delete"  onClick= {e => this.delFile(file)} >delete</button> </li>
+                                return <li><DescriptionIcon/> {file} <button name="Delete"  onClick= {e => this.delFile(file)} >delete</button> </li>
+                                
                             }
                             else{
-                                return <li > <FolderIcon/> {file} <button  name="Delete" onClick= {e => this.delFile(file)} >delete</button> <Filetree files = {files[file]} /></li>
+                                return (<ExpansionPanel expanded={true} style={{backgroundColor: "lightgrey"}}  > 
+                                <li > <FolderIcon/> {file}
+
+                                {/* <form onSubmit={this.handleSubmit} style={{ paddingLeft: '20px' , paddingBottom: '17px', paddingTop: '7px'}} >
+                                    <label>
+                                    new name:
+                                    <input type="text" newName={this.state.newName} onChange={this.handleChangeRename} />
+                                        </label>
+                                        <button type="submit" name="Rename" onClick= {this.renameFile(file)} >Rename</button>
+                                </form> */}
+
+
+                                 <button  name="Delete" onClick= {e => this.delFile(file)} >delete</button>  <ExpansionPanelDetails> <Filetree files = {files[file]} /> </ExpansionPanelDetails></li>
+                                </ExpansionPanel>)
                             }
 
                         })
                     }
                 </ul>
+
+                </Grid>
+                
              </div>
         );
      }
